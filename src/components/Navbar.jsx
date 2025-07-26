41a0d8cd0ce4a6997f0dc763012e844765430f93
MariamSawera <mariamsawera@gmail.com>
2025-07-26 15:22:04 -0800

    added navbar.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-// src/components/Navbar.jsx
+
 import { Link } from "react-router-dom";
+import "./Navbar.css"; 
 
 export default function Navbar() {
   return (
-    <nav className="p-4 bg-black text-white flex justify-between">
-      <h1 className="text-xl font-bold">Musix</h1>
-      <div className="space-x-4">
+    <nav className="navbar">
+      <h1 className="logo">Musix</h1>
+      <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/chat">Chat</Link>
       </div>
